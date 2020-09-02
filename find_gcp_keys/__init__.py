d8561d58f28805ab4737f2ffa5b7d2cc1ca24d03
Denis Loginov <denis@broadinstitute.org>
2020-09-01 23:07:27 -0400

    Fix find_valid_keys() export

--- find_gcp_keys/__init__.py
+++ find_gcp_keys/__init__.py
@@ -2,4 +2,4 @@
 Finds and reports valid Google Service Account keys on your filesystem
 """
 
-from find_gcp_keys.__main__ import find_key_paths, is_valid_key, search
+from find_gcp_keys.__main__ import find_key_paths, find_valid_keys, is_valid_key
